a9c7d2cdeca0ed3c62319038180a93fbb5d62e89
listimdn10 <quynhnhu170218@gmail.com>
2024-07-07 11:42:56 +0700

    route update and delete

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express"; 
+import express, { response } from "express"; 
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Ticket } from "./dtb_models/ticketModel.js";
@@ -45,19 +45,19 @@ app.post('/tickets', async (request, response)=> {
     }
 });
 
-// //route to GET ALL bookS from dtb 
-// app.get('/tickets', async (request, response)=>{
-//     try {
-//         const tickets = await Ticket.find({});
-//         return response.status(200).json({
-//             count: tickets.length,
-//             data: tickets
-//         })
-//     } catch (error){
-//         console.log(error.message); 
-//         response.status(500).send({ message: error.message});
-//     }
-// }); 
+//route to GET ALL bookS from dtb 
+app.get('/tickets', async (request, response)=>{
+    try {
+        const tickets = await Ticket.find({});
+        return response.status(200).json({
+            count: tickets.length,
+            data: tickets
+        })
+    } catch (error){
+        console.log(error.message); 
+        response.status(500).send({ message: error.message});
+    }
+}); 
 
 
 //route to GET ONE tickets from dtb BY ID
@@ -76,23 +76,69 @@ app.get('/tickets/:id', async (request, response)=>{ //the /:id required to plac
 // Route to GET ONE ticket by NAME  
 //params is parameter which is what you include in your request 
 //query is your fields which is what you type in the query params with key is name and value is "place-here-the-name"
-app.get('/tickets', async (request, response) => {
+// app.get('/tickets', async (request, response) => {
+//     try {
+//         const { name } = request.query;
+
+//         if (!name) {
+//             return response.status(400).send({ message: 'Name query parameter is required' });
+//         }
+
+//         const ticket = await Ticket.findOne({ name });
+//         if (!ticket) {
+//             return response.status(404).send({ message: 'Ticket not found' });
+//         }
+
+//         return response.status(200).json(ticket);
+//     } catch (error) {
+//         console.log(error.message);
+//         response.status(500).send({ message: error.message });
+//     }
+// });
+
+
+
+//route for update a ticket 
+app.put('/tickets/:id', async (request, response) => {
     try {
-        const { name } = request.query;
-
-        if (!name) {
-            return response.status(400).send({ message: 'Name query parameter is required' });
+        if(
+            !request.body.name ||
+            !request.body.departure ||
+            !request.body.arrival ||
+            !request.body.class
+        ){
+            return response.status(400).send({
+                message: 'send all required fields', //only when want to update all fields
+            });
         }
-
-        const ticket = await Ticket.findOne({ name });
-        if (!ticket) {
-            return response.status(404).send({ message: 'Ticket not found' });
+        const { id } = request.params;
+        const result = await Ticket.findByIdAndUpdate(id, request.body)
+        
+        if(!result) {
+            return response.status(200).json({message: 'Ticket not found'});
         }
+        
+        return response.status(200).send({message: 'Ticket updated successfully'});
+    } catch (error){
+        console.log(error.message); 
+        response.status(500).send({message: error.message});
+    }
+})
 
-        return response.status(200).json(ticket);
-    } catch (error) {
+//route to delete a ticket 
+app.delete('/tickets/:id', async (request, response) => {
+    try{
+        const { id } = request.params; 
+
+        const result = await Ticket.findByIdAndDelete(id); 
+
+        if(!result){
+            return response.status(404).json({message: 'ticket not found'});
+        }
+        return response.status(200).send({message: 'ticket deleted'});
+    }catch (error) {
         console.log(error.message);
-        response.status(500).send({ message: error.message });
+        response.status(500).send({message: error.message});
     }
 });
 
