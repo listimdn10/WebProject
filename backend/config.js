2e8f93edf5880b2d0e3a8cdc067d1ff12ccac952
listimdn10 <quynhnhu170218@gmail.com>
2024-07-09 20:54:30 +0700

    test

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = 5555; //to listen
 
 export const mongoDBURL = 'mongodb+srv://root:895252426Nhu@buildyourwebsite.qyvsnze.mongodb.net/ticket-Collection?retryWrites=true&w=majority&appName=BuildYourWebsite' 
 
